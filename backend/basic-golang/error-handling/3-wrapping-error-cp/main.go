15896630c1b819a3de544d59f24bafc1809a2265
putrikarr <putrikartikanuriani@gmail.com>
2022-03-31 21:13:31 +0700

    edit : wrapping error

--- backend/basic-golang/error-handling/3-wrapping-error-cp/main.go
+++ backend/basic-golang/error-handling/3-wrapping-error-cp/main.go
@@ -22,6 +22,7 @@ func GetAge(data map[string]int, name string) (int, error) {
 func IsEligibleToVaccine(data map[string]int, name string) (bool, error) {
 	age, err := GetAge(data, name)
 	if err != nil {
+		return false, fmt.Errorf("error in IsEligibleToVaccine : %w", err)
 		// TODO: answer here
 		//return false, fmt.Println()
 	}
