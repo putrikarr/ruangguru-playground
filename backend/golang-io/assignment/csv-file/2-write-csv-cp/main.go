a027474ebcb59d6df74df093d021d6104c5d5e39
putrikarr <putrikartikanuriani@gmail.com>
2022-04-05 23:04:26 +0700

    edit : 2-write-csv-cp

--- backend/golang-io/assignment/csv-file/2-write-csv-cp/main.go
+++ backend/golang-io/assignment/csv-file/2-write-csv-cp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"log"
 	"os"
 	"strconv"
 )
@@ -12,12 +13,29 @@ type Menu struct {
 }
 
 func WriteToCSV(fileName string, records []Menu) error {
-	csvFile, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer csvFile.Close()
-
 	// TODO: answer here
+	csvFile, err := os.Create("menu.csv")
+	if err != nil {
+		log.Fatalf("cannot create file %q: %s\n", "menu.csv", err)
+	}
+
+	defer csvFile.Close()
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
+
+	records = []Menu{
+		{"Pizza", 100},
+		{"Burger", 200},
+		{"Coffee", 300},
+		{"Tea", 400},
+		{"Sandwich", 500},
+	}
+
+	for _, record := range records {
+		row := []string{record.Name, strconv.Itoa(record.Price)}
+		if err := csvWriter.Write(row); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
 	return nil
 }
