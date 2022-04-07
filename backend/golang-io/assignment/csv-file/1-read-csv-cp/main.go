33893b8ea7716fd5280b2e451fe5da41735e6849
putrikarr <putrikartikanuriani@gmail.com>
2022-04-08 00:35:27 +0700

    edit : read csv cp

--- backend/golang-io/assignment/csv-file/1-read-csv-cp/main.go
+++ backend/golang-io/assignment/csv-file/1-read-csv-cp/main.go
@@ -30,5 +30,30 @@ func CSVToMap(data map[string]string, fileName string) (map[string]string, error
 
 	return data, nil
 	// TODO: answer here
+	f, err := os.Open(fileName)
+	if err != nil {
+		return data, err
+	}
+
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	// skip baris pertama (nama kolom)
+
+	if _, err := r.Read(); err != nil {
+		return data, err
+	}
+
+	dataToMap, err := r.ReadAll()
+	if err != nil {
+		return data, err
+	}
+
+	for _, record := range dataToMap {
+		data[record[0]] = record[1]
+	}
+
+	return data, nil
 
 }
