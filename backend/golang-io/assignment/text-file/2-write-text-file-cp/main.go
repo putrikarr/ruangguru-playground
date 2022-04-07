0301f9ea04ac9174f2147d61bb6db3023807f92c
putrikarr <putrikartikanuriani@gmail.com>
2022-04-08 00:25:39 +0700

    edit : write text file cp

--- backend/golang-io/assignment/text-file/2-write-text-file-cp/main.go
+++ backend/golang-io/assignment/text-file/2-write-text-file-cp/main.go
@@ -8,5 +8,24 @@ import (
 // dalam test ini terdapat fungsi os.Remove ya. itu automatis nge remove file yang telah dibuat
 // Untuk keperluan testing
 func WriteFile(fileName string, fileData string) error {
-	return nil // TODO: replace this
+	//return nil // TODO: replace this
+	//membuat file
+	file, err := os.Create(fileName)
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+		return err
+	}
+
+	//menutup file sebelum fungsi selesai dijalankan
+	defer file.Close()
+
+	_, err = file.WriteString(fileData)
+
+	if err != nil {
+		log.Fatalf("failed writing to file: %s", err)
+		return err
+	}
+
+	return nil
 }
