cd350442f9240b0e7a70185a1a788ab6337f3998
putrikarr <putrikartikanuriani@gmail.com>
2022-04-11 14:51:51 +0700

    edit : data-structure\array\search cp

--- backend/data-structure/array/2-search-cp/main.go
+++ backend/data-structure/array/2-search-cp/main.go
@@ -15,5 +15,17 @@ func main() {
 }
 
 func SearchMatch(arr1 []string, arr2 []string) ([]string, error) {
-	return []string{}, nil // TODO: replace this
+	//return []string{}, nil // TODO: replace this
+	var result []string
+	for _, v := range arr1 {
+		for _, v2 := range arr2 {
+			if v == v2 {
+				result = append(result, v)
+			}
+		}
+	}
+	if len(result) == 0 {
+		return []string{}, fmt.Errorf("no match")
+	}
+	return result, nil
 }
