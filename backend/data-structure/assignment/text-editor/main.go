cbf12b563efc6d8ce8567737f42bbf304d592946
putrikarr <putrikartikanuriani@gmail.com>
2022-04-29 11:37:23 +0700

    ad an answer of assignment\text-editor

--- backend/data-structure/assignment/text-editor/main.go
+++ backend/data-structure/assignment/text-editor/main.go
@@ -26,17 +26,48 @@ func NewTextEditor() *TextEditor {
 }
 
 func (te *TextEditor) Write(ch rune) {
+	if te.UndoStack.IsEmpty() {
+		te.UndoStack.Push(ch)
+		fmt.Println("empty")
+	} else {
+		lastChar, _ := te.UndoStack.Peek()
+		if lastChar != ch {
+			te.UndoStack.Push(ch)
+			fmt.Println("Write:", string(te.UndoStack.Data))
+		}
+	}
 	// TODO: answer here
 }
 
 func (te *TextEditor) Read() []rune {
+	if te.UndoStack.IsEmpty() {
+		fmt.Println("empty")
+		return nil
+	} else {
+		ch, _ := te.UndoStack.Pop()
+		te.UndoStack.Push(ch)
+		fmt.Println("Read:", string(te.UndoStack.Data))
+		return te.UndoStack.Data
+	}
 	// TODO: answer here
 }
 
 func (te *TextEditor) Undo() {
+	if te.UndoStack.IsEmpty() {
+		return
+	} else {
+		ch, _ := te.UndoStack.Pop()
+		te.RedoStack.Push(ch)
+	}
 	// TODO: answer here
 }
 
 func (te *TextEditor) Redo() {
+	if te.RedoStack.IsEmpty() {
+		return
+	} else {
+		ch, _ := te.RedoStack.Pop()
+		te.UndoStack.Push(ch)
+	}
 	// TODO: answer here
 }
