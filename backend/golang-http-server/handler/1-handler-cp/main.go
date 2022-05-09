e2b0dd4cf7daa97cd97b14abc14e40e734e5b876
putrikarr <putrikartikanuriani@gmail.com>
2022-05-09 22:01:58 +0700

    add an answer of golang-http-server\handler\1-handler-cp

--- backend/golang-http-server/handler/1-handler-cp/main.go
+++ backend/golang-http-server/handler/1-handler-cp/main.go
@@ -10,5 +10,9 @@ import (
 // Hint, gunakan time.Weekday, time.Day, time.Month, dan time.Year
 
 func GetHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {} // TODO: replace this
+	//return func(writer http.ResponseWriter, request *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		fmt.Fprintf(w, "%s, %d %s %d", t.Weekday(), t.Day(), t.Month(), t.Year())
+	}
 }
