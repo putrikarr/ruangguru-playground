af6093e65c636654a9cff777893be70f282ebf1b
putrikarr <putrikartikanuriani@gmail.com>
2022-05-10 12:58:01 +0700

    add an answer of golang-http-server\handler\4-chaining-middleware-cp

--- backend/golang-http-server/handler/4-chaining-middleware-cp/main.go
+++ backend/golang-http-server/handler/4-chaining-middleware-cp/main.go
@@ -20,13 +20,36 @@ func TimeHandler() http.HandlerFunc {
 }
 
 func RequestMethodGetMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}) // TODO: replace this
+	//return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}) // TODO: replace this
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func AdminMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}) // TODO: replace this
+	//return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}) // TODO: replace this
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("role") != "ADMIN" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	// TODO: answer here
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: RequestMethodGetMiddleware(AdminMiddleware(TimeHandler())),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
