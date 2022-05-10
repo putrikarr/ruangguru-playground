3ef64a3c0bd711b96b6baaad7125550236678423
putrikarr <putrikartikanuriani@gmail.com>
2022-05-10 13:45:23 +0700

    add an answer of golang-http-server\multiplexing\1-servemux-cp

--- backend/golang-http-server/multiplexing/1-servemux-cp/main.go
+++ backend/golang-http-server/multiplexing/1-servemux-cp/main.go
@@ -11,16 +11,30 @@ import (
 // Route kedua yaitu "/hello" yang menghandle SayHelloHandler
 
 func TimeHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO: replace this
+		t := time.Now()
+		output := fmt.Sprintf("%v, %v %v %v", t.Weekday(), t.Day(), t.Month(), t.Year())
+		fmt.Fprint(w, output)
+	}
 }
 
 func SayHelloHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) { // TODO: replace this
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			fmt.Fprint(w, "Hello there")
+			return
+		}
+		output := fmt.Sprintf("Hello, %v!", name)
+		fmt.Fprint(w, output)
+	}
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	// TODO: answer here
+	mux.HandleFunc("/time", TimeHandler())
+	mux.HandleFunc("/hello", SayHelloHandler())
 	return mux
-
 }
