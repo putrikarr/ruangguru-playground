071c4d145f9de40bca4a4b47ed1884a8045555f7
putrikarr <putrikartikanuriani@gmail.com>
2022-05-11 09:01:14 +0700

    add an answer of golang-http-server\gin\4-query-string-cp

--- backend/golang-http-server/gin/4-query-string-cp/main.go
+++ backend/golang-http-server/gin/4-query-string-cp/main.go
@@ -11,7 +11,26 @@ func GetRouter() *gin.Engine {
 
 	// TODO: answer here
 
+	// buat router method GET dengan path /movie
+
+	// ada 2 query parameter yaitu: genre dan country
+	// buat defaultquery kalau genre tidak dikirim
+	// buat query kalau parameter country tidak dikirim maka return StatusOK tanpa menampilkan data
+	// buat query kalau parameter country dikirim maka menampilkan data
+
+	router.GET("/movie", func(c *gin.Context) {
+		genre := c.DefaultQuery("genre", "general")
+		country := c.Query("country")
+
+		if country == "" {
+			c.String(http.StatusOK, "Here result of query of movie with genre %s", genre)
+			return
+		}
+		c.String(http.StatusOK, "Here result of query of movie with genre %s in country %s", genre, country)
+	})
+
 	return router
+
 }
 
 func main() {
